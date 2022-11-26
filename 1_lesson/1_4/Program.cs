1ded48f9d40b56fefec96dc0720e6ced277849cf
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-11-26 19:57:49 +0700

    tast 1_4 added

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает
+// все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Write a number: ");
+int num = int.Parse(Console.ReadLine());
+int counter = -num;
+
+while (counter <= num)
+{
+    Console.Write(counter + " " );
+    counter++;
+}
+
