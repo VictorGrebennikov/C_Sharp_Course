a40db254703fde5281fd47c8fe9bd7439fdca938
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-11-26 23:23:19 +0700

    task 1_2 added in homework dir

---
+++ homework/1_lesson/1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №3.
+// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("Nope");
+}
