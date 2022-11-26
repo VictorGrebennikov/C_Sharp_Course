3cd5b72f2698f14f10e066c0aa0303cf19a1a6af
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-11-26 18:21:10 +0700

    task 1_0

--- 1_lesson/1_0/Program.cs
+++ 1_lesson/1_0/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Напишите программу , которая на вход принимает число и
+//    выдает его квадрат (число умноженное само на себя).
+
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(num * num);
