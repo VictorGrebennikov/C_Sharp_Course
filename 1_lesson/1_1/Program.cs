7adcf929fc9764a369505d1c1ac6d57261230e88
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-11-26 19:32:26 +0700

    1_1 and 1_2 tasks added

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №1. 
+// Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+
+Console.WriteLine("Write first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("Yes");
+}
+
+else
+
+    Console.WriteLine("No");
+
---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №2.
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+
+Console.WriteLine("Enter the number of the day of the week: ");
+int num = int.Parse(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("!There can't be more than 7 days in a week!");    
+        break;
+}
