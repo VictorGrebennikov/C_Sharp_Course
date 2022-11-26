9ae76c26e81e49d2cefa507dd381c6adb64a6caa
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-11-26 23:37:08 +0700

    task 1_3 added in homework dir

---
+++ homework/1_lesson/1_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №4.
+// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+int counter = 1;
+
+while (counter <= num)
+{
+    if (counter % 2 == 0)
+    {
+        Console.Write(counter + " ");
+    }
+    counter++;
+}
