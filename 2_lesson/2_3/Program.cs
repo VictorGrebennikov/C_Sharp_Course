a38755dbdd4e443b34c9a182c67bf1d8324beb43
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-11-29 23:09:31 +0700

    Task 2_3

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+string Divisible_7_23(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+        return "Divisible";
+    else
+        return "Not divisible";
+}
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(Divisible_7_23(number));
