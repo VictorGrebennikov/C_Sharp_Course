cff25c2022d112ea112816255d390b8ccc534bf3
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-01 00:48:37 +0700

    Task 2_0 added

---
+++ homework/2_lesson/2_0/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1:
+// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+void TheSecondDigit(int num)
+{
+    while (true)
+    {
+        if (Math.Abs(num) < 100 || Math.Abs(num) > 1000)
+            {
+                Console.WriteLine("A three digit number must be entered: ");
+                num = int.Parse(Console.ReadLine());
+            } 
+        else
+            {
+                Console.WriteLine($"The second digit is {Math.Abs((num / 10) % 10)}");
+                break;
+            }
+    }
+   
+}
+
+Console.WriteLine("Enter a three-digit number: ");
+int entered_num = int.Parse(Console.ReadLine());
+TheSecondDigit(entered_num);
