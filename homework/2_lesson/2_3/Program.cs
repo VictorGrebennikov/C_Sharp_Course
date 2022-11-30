09ee805ede2964df1e1e3de084636377f35261ae
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-01 00:56:53 +0700

    Task 2_3 added

---
+++ homework/2_lesson/2_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4:
+// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void DayOfTheWeek (int num)
+{
+    if (num > 0 && num <= 5)
+    {
+        Console.WriteLine("It's a weekday.");
+    }
+    else if (num == 6 || num == 7)
+    {
+        Console.WriteLine("It's a day off.");
+    }
+    else if (num > 7 || num <=0)
+    {
+        Console.WriteLine("Invalid number");
+    }
+}
+
+Console.WriteLine("Enter the number of the day of the week: ");
+int entered_num = int.Parse(Console.ReadLine());
+DayOfTheWeek(entered_num);
