63834fcf7db15061bada7652f219485e4bf28ce0
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-01 00:51:31 +0700

    Task 2_1 added

---
+++ homework/2_lesson/2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2:
+// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int CutSecondDigit(int num)
+{
+    Console.WriteLine(num);
+
+    return (num / 100) * 10 + (num % 10);
+}
+
+Console.WriteLine(CutSecondDigit(new Random().Next(100, 1000)));
