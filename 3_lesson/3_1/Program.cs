a7c6935217743818e9ccd8ec15575aef5b584f29
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-04 21:40:17 +0700

    Task 3_1

---
+++ 3_lesson/3_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Quarters(string a)
+{
+    if (a == "I")
+        Console.WriteLine("x > 0 && y > 0");
+    else if (a == "II")
+        Console.WriteLine("x < 0 && y > 0");
+    else if (a == "III")
+        Console.WriteLine("x < 0 && y < 0");
+    else if (a == "IV")
+        Console.WriteLine("x > 0 && y < 0");
+    else
+        Console.WriteLine("error");
+}
+
+Quarters(Console.ReadLine());
