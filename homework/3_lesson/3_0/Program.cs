17277ff1dce2ec02817dad2937f19d4cc4468ddf
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-04 23:33:53 +0700

    HW task 3_0 added

---
+++ homework/3_lesson/3_0/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №0.
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void Palindrome(int num)
+{
+    while (true)
+    {
+        if (Math.Abs(num) < 10000 || Math.Abs(num) > 100000)
+            {
+                Console.WriteLine("A five digit number must be entered: ");
+                num = int.Parse(Console.ReadLine());
+            } 
+        else
+            {
+                if (num % 10 == num / 10000 && (num % 100) / 10 == (num / 1000) % 10 )
+                {
+                    Console.WriteLine($"The number {num} is palindrome!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"The number {num} is NOT palindrome!");
+                    break;
+                }
+            }
+    }
+}
+
+Console.WriteLine("Enter a five-digit number: ");
+int entered_num = int.Parse(Console.ReadLine());
+Palindrome(entered_num);
