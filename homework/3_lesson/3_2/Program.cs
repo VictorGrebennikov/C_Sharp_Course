e711ef3597a7b8cf3c4bc98ee6daecb5b6c9f869
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-05 00:12:55 +0700

    HW task 3_2 added

---
+++ homework/3_lesson/3_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Cubes(int N)
+{
+    int i = 0;
+    double result;
+    while (i < N)
+    {
+       i ++;
+       result = Math.Pow(i, 3);
+       Console.WriteLine(result);
+    }
+}
+
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+Cubes(num);
