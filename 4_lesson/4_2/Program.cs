e465960c99cf41814c34f94101267bc62ebd5b0d
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-07 19:04:58 +0700

    Task 4_0, 4_1, 4_2, 4_3 added

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чсел от 1 до N.
+
+int ProductNums(int num)
+{
+    int all_product = 1;
+    for (int i = 1;i <= num; i++)
+    {
+        all_product *= i;
+    }
+
+    return all_product;
+}
+
+Console.WriteLine(ProductNums(int.Parse(Console.ReadLine())));
