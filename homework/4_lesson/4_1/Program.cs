fdbdd84279d0b8052e141c345811dcbb76b3d40f
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-07 23:41:46 +0700

    HW task 4_1 added

---
+++ homework/4_lesson/4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int NumCount(int n)
+{
+    
+    int count = 0;
+    int result = 0;
+
+    while (n > 0)
+    {
+        result = result + (n % 10);
+        n = n / 10;
+        count++;
+    }
+
+    return result;
+}
+
+Console.Write("Enter a number: ");
+Console.Write(NumCount(int.Parse(Console.ReadLine())));
