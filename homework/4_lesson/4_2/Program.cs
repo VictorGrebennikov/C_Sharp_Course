40c9a6daefd30087c89453e6a56d0e6fd6c21920
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-07 23:45:10 +0700

    HW task 4_2 added

---
+++ homework/4_lesson/4_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray(int n)
+{
+    int[] array = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] arr = CreateArray(int.Parse(Console.ReadLine()));
+PrintArray(arr);
