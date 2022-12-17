2941f8c395d5cd07173c7713a58445ab8ebd48d2
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-17 22:55:31 +0700

    HW task 7_2 added

---
+++ homework/7_lesson/7_2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintArray(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] CreateArray2(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void AverageCountColumn(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    Console.Write($"Average value of columns: ");
+    double count_average = 0;
+
+    for (int j = 0; j < column_size; j++)
+    {
+        count_average = 0;
+
+        for (int i = 0; i < row_size; i++)
+            count_average += arr[i, j];
+        Console.Write($" {count_average}");
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = CreateArray2(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+PrintArray(arr_1);
+
+AverageCountColumn(arr_1);
