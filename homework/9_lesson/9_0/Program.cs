1f7f6bc6a7789aa485450f37709a849a219d1a6c
Victor Grebennikov <victorgrebennikov4@gmail.com>
2022-12-25 23:24:22 +0700

    HW task 9_0 added

---
+++ homework/9_lesson/9_0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Задайте значение N. Напишите программу, которая выведет все четные натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void Numbers(int n)
+{
+    if (n <= 1) return;
+
+    if (n % 2 != 0) n -= 1;
+
+    Console.Write($"{n} ");
+    Numbers(n - 1);
+}
+
+Console.WriteLine("Enter a number: ");
+int n = int.Parse(Console.ReadLine());
+
+Numbers(n);
